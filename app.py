57b32d328d7635ca73d77154461e6eb52caeee91
SalmaAlbakr <salalbakr19988@gmail.com>
2026-06-18 12:04:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -118,8 +118,13 @@ def predict():
     })
 
 
-# =========================
-# Run Server
-# =========================
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello"
+
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
